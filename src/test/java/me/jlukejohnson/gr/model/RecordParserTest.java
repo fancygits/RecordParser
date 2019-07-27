71c9c5067049443af87a36315c5f40fd02b6a979
James Luke Johnson <jjohn144@my.westga.edu>
2019-07-26 22:43:15 -0500

    Added more tests to improve code coverage of RecordParser.

--- src/test/java/me/jlukejohnson/gr/model/RecordParserTest.java
+++ src/test/java/me/jlukejohnson/gr/model/RecordParserTest.java
@@ -31,14 +31,61 @@ class RecordParserTest {
 		writer.flush();
 		writer.close();
 		temp.deleteOnExit();
-		RecordParser importer = new RecordParser();
+		RecordParser parser = new RecordParser();
 		try {
-			assertTrue(importer.importRecords(temp));
-			assertEquals(3, importer.getPeople().size());
-			assertEquals("Costley        Dukie          male      Green       7/13/1947 ", importer.getPeople().get(0).getDetails());
+			assertTrue(parser.importRecords(temp));
+			assertEquals(3, parser.getPeople().size());
+			assertEquals("Costley        Dukie          male      Green       7/13/1947 ", parser.getPeople().get(0).getDetails());
 		} catch (FileNotFoundException fnfe) {
 			fnfe.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Test to confirm that importRecords will throw an exception for unfound files
+	 */
+	@Test
+	void testImportRecordsShouldThrowExceptionOnInvalidFile() {
+		RecordParser parser = new RecordParser();
+		assertThrows(FileNotFoundException.class, () -> {
+			parser.importRecords(new File("unknown.txt"));
+		});
+	}
+	
+	/**
+	 * Tests that importRecords returns false on a failed import
+	 * @throws IOException 
+	 */
+	@Test
+	void testImportRecordsShouldReturnFalseIfALineHasTooManyWords() throws IOException {
+		File badData = File.createTempFile("badData", "tmp");
+		FileWriter writer = new FileWriter(badData);
+		writer.write("This is a sentence and cannot be parsed to a person because it's too long.");
+		writer.flush();
+		writer.close();
+		badData.deleteOnExit();
+		RecordParser parser = new RecordParser();
+		try {
+			assertFalse(parser.importRecords(badData));
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Test to confirm that a file without sufficient data will not create a person
+	 * @throws IOException
+	 */
+	@Test
+	void testImportRecordsShouldThrowIllegalArgumentExceptionWhenNotEnoughData() throws IOException {
+		RecordParser parser = new RecordParser();
+		File notEnoughData = File.createTempFile("notEnoughData", "tmp");
+		FileWriter writer = new FileWriter(notEnoughData);
+		writer.write("Cruise, Tom, Blue");
+		writer.flush();
+		writer.close();
+		notEnoughData.deleteOnExit();
+		assertFalse(parser.importRecords(notEnoughData));
 	}
 
 }
